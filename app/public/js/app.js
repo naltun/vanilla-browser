3852e11f5d75a1ebc791372a74178e72a3e99bf0
Noah <nba4191@gmail.com>
2016-01-17 06:21:10 +0000

    Add angular, bootstrap, app configurations

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,9 +1,2 @@
-angular.module('app', [])
-    .controller('main', function($scope) {
-        $scope.people = [];
-        $scope.person = {};
-        $scope.add = function() {
-            $scope.people.push($scope.person);
-            $scope.person = {};
-        };
-    });
+angular.module('browser', ['ui.bootstrap', 'ngSanitize'])
+       .controller('main', function(){});
